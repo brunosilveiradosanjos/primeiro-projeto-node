8f111304d442152eed4fde1e363314b18cbec3ba
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-09-12 00:55:36 -0300

    Atualizando avatar

--- src/models/User.ts
+++ src/models/User.ts
@@ -17,6 +17,9 @@ class Users {
     @Column()
     password: string;
 
+    @Column()
+    avatar: string;
+
     @CreateDateColumn()
     created_at: Date;
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import CreateUserService from '../service/CreateUserService'
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import uploadConfig from '../config/upload';
+import UpdateUserAvatarService from '../service/UpdateUserAvatarService'
 // Rota: Receber a requisição, chamar outro arquivo, devolver uma resposta
 
 const usersRouter = Router();
@@ -35,7 +36,22 @@ usersRouter.patch(
     ensureAuthenticated,
     upload.single('avatar'),
     async (request, response) => {
-        return response.json({ ok: true })
+        try {
+            const updateUserAvatar = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFilename: request.file.filename
+            })
+
+            delete user.password;
+
+            return response.json(user);
+
+        } catch (err) {
+            return response.status(400).json({ error: err.message })
+        }
+
     })
 
 export default usersRouter;
