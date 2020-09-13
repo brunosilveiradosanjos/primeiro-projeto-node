c39e3c0c3603957de9eaa13713a7a764b677eaf2
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-09-13 20:45:10 -0300

    Lidando com erros

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,23 +5,19 @@ import AuthenticateUserService from '../service/AuthenticateUserService';
 const sessionRouter = Router();
 
 sessionRouter.post('/', async (request, response) => {
-    try {
+    const { email, password } = request.body;
 
-        const { email, password } = request.body;
+    const authenticateUser = new AuthenticateUserService();
 
-        const authenticateUser = new AuthenticateUserService();
+    const { user, token } = await authenticateUser.execute({
+        email,
+        password
+    });
 
-        const { user, token } = await authenticateUser.execute({
-            email,
-            password
-        });
+    delete user.password;
 
-        delete user.password;
+    return response.json({ user, token });
 
-        return response.json({ user, token });
-    } catch (error) {
-        return response.status(error.statusCode).json({ error: error.message })
-    }
 })
 
 export default sessionRouter;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -36,21 +36,17 @@ usersRouter.patch(
     ensureAuthenticated,
     upload.single('avatar'),
     async (request, response) => {
-        try {
-            const updateUserAvatar = new UpdateUserAvatarService();
 
-            const user = await updateUserAvatar.execute({
-                user_id: request.user.id,
-                avatarFilename: request.file.filename
-            })
+        const updateUserAvatar = new UpdateUserAvatarService();
 
-            delete user.password;
+        const user = await updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFilename: request.file.filename
+        })
 
-            return response.json(user);
+        delete user.password;
 
-        } catch (err) {
-            return response.status(400).json({ error: err.message })
-        }
+        return response.json(user);
 
     })
 
